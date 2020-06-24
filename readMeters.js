01859e66173590e892255de646ea595776bbf0f3
everzet <ever.zet@gmail.com>
2020-06-24 19:50:26 +0100

    Make metersScript path relative, so we can require

--- readMeters.js
+++ readMeters.js
@@ -1,5 +1,7 @@
+const path = require("path");
+
 module.exports = async (metersScript) => {
-  const meters = require(metersScript);
+  const meters = require(path.join('./', metersScript));
   const promises = [];
   const readings = {};
 
