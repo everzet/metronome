3252efab2bfbaca1a9ad8d2a52541094008334ab
everzet <ever.zet@gmail.com>
2020-06-27 15:43:55 +0100

    Simplify e2e test dates to increase focus on dates themselves

--- cli-tool/e2e.test.js
+++ cli-tool/e2e.test.js
@@ -4,7 +4,7 @@ const initializeTestRepository = require("./initializeTestRepository");
 let repo;
 beforeEach(() => {
   repo = initializeTestRepository({
-    date: "2018-06-26T10:46:19+01:00",
+    date: "2018-06-26 00:00",
   });
 });
 
@@ -20,22 +20,22 @@ net_promoter_score = 3
 desktop_conversion_rate = 1.2
 employee_feeling = "unhappy"
   `,
-    date: "2018-06-26T10:00:19+01:00",
+    date: "2018-06-26 00:00",
   });
   // set first expectation
   repo.commit(
     "[meter-expectation: desktop_conversion_rate will increase by 30% in 2 weeks ]",
-    { date: "2018-06-27T10:00:19+01:00" }
+    { date: "2018-06-27 00:00" }
   );
   // set second expectation
   repo.commit(
     "[meter-expectation: net_promoter_score will increase to 8 in 1 month ]",
-    { date: "2018-06-28T10:00:19+01:00" }
+    { date: "2018-06-28 00:00" }
   );
   // set third expectation
   repo.commit(
     `[meter-expectation: employee_feeling will become 'happy' in 2 weeks ]`,
-    { date: "2018-06-29T10:00:19+01:00" }
+    { date: "2018-06-29 00:00" }
   );
   // update metrics file
   repo.commit("[meter-readings:master]", {
@@ -44,7 +44,7 @@ net_promoter_score = 4
 desktop_conversion_rate = 1.6
 employee_feeling = "concerned"
   `,
-    date: "2018-07-05T10:00:19+01:00",
+    date: "2018-07-05 00:00",
   });
   // update metrics file
   repo.commit("[meter-readings:master]", {
@@ -53,7 +53,7 @@ net_promoter_score = 6
 desktop_conversion_rate = 1.65
 employee_feeling = "concerned"
   `,
-    date: "2018-07-14:00:19+01:00",
+    date: "2018-07-14 00:00",
   });
 
   // run analysis
